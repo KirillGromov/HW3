0cfb36aeccdcefe833253154e31ae7e1cd7957df
KirillGromov <kirill.gromow@yandex.ru>
2022-10-23 23:11:34 +0300

    git init

---
+++ task19/Program.cs
@@ -0,0 +1,26 @@
+﻿
+string a = Console.ReadLine();
+
+char[] ch = new char[a.Length]; 
+char[] chRev = new char[a.Length];
+    
+for (int i = 0; i < a.Length; i++) { 
+    ch[i] = a[i]; 
+    chRev[i] = a[i];
+} 
+
+Array.Reverse(ch);
+
+int count = 0;
+
+for (int i = 0; i < a.Length; i++) { 
+     if(ch[i] == chRev[i]){
+        count++;
+     }
+}
+if(count == a.Length){
+    Console.WriteLine("Полиндром");
+}else{
+    Console.WriteLine("Не полиндром");
+}
+
---
+++ task21/Program.cs
@@ -0,0 +1,37 @@
+﻿
+int a1, b1, c1, a2, b2, c2;
+
+Console.WriteLine("Введите a1: ");
+a1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите a2: ");
+a2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите b1: ");
+b1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите b2: ");
+b2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите c1: ");
+c1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите c2: ");
+c2 = int.Parse(Console.ReadLine());
+
+double sq1 = Math.Pow((a2-a1), 2);
+double sq2 = Math.Pow((b2-b1), 2);
+double sq3 = Math.Pow((c2-c1), 2);
+
+double sq_res = sq1+sq2+sq3;
+
+Console.WriteLine((float)Math.Sqrt(sq_res));
+
+
+
+
+
+
+
+
+
